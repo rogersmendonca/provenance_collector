44e0e769acdbe4bc2cbb9155fea575fb054cf929
Rogers Reiche de Mendonca <rogers.rj@gmail.com>
2013-12-05 23:38:49 -0200

    Atualizacao do retrosp_workflow.sql

--- kettle/pdi_cnpq_rnp/SQL/retrosp_workflow.sql
+++ kettle/pdi_cnpq_rnp/SQL/retrosp_workflow.sql
@@ -4,6 +4,7 @@ SELECT
 , t1.id_workflow
 , t1.start_date
 , t1.finish_date
+, t1.id_user
 , t1.success
 , t1.id_root_prosp_repository
 , t1.id_root_prosp_workflow
@@ -14,9 +15,12 @@ SELECT
 , t1.id_parent
 , (SELECT t4.name FROM prosp_workflow t4 WHERE t4.id_repository = t1.id_parent_prosp_repository AND t4.id_workflow = t1.id_parent_prosp_workflow) as parent_name
 , t2.name
-FROM retrosp_workflow t1, prosp_workflow t2
-WHERE t1.id_root_prosp_repository = 1
-AND t1.id_root_prosp_workflow = 1
-AND t1.id_root = 7
+, t5.login
+FROM retrosp_workflow t1, prosp_workflow t2, user t5
+WHERE t1.id_root_prosp_repository = ?
+AND t1.id_root_prosp_workflow = ?
+AND t1.id_root = ?
 AND t2.id_repository = t1.id_prosp_repository
 AND t2.id_workflow = t1.id_prosp_workflow
+AND t5.id_repository = t1.id_prosp_repository
+AND t5.id_user = t1.id_user
